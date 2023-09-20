01c1c4c65b4007768099f99611c7657c8ec92db0
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2023-09-20 01:36:33 +0100

    initial commit

---
+++ test.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+ran_tests=1
+
+if [ -z "$1" ]; then
+    echo "Running all tests"
+    for f in instances/*/*.in.json; do
+        echo -n "  Running $(basename ${f/.in.json/})..."
+        python proj.py $f ${f/.in.json/.out.json}
+        echo "Done"
+    done
+elif [ $# == 1 ]; then
+    if [ $1 == "easy" ]; then
+        echo "Running easy tests"
+        for f in instances/easy/*.in.json; do
+            echo -n "  Running $(basename ${f/.in.json/})..."
+            python proj.py $f ${f/.in.json/.out.json}
+            echo "Done"
+        done
+    elif [ $1 == "medium" ]; then
+        echo "Running medium tests"
+        for f in instances/medium/*.in.json; do
+            echo -n "  Running $(basename ${f/.in.json/})..."
+            python proj.py $f ${f/.in.json/.out.json}
+            echo "Done"
+        done
+    elif [ $1 == "hard" ]; then
+        echo "Running hard tests"
+        for f in instances/hard/*.in.json; do
+            echo -n "  Running $(basename ${f/.in.json/})..."
+            python proj.py $f ${f/.in.json/.out.json}
+            echo "Done"
+        done
+    elif [ $1 == "custom" ]; then
+        echo "Running custom tests"
+        for f in instances/custom/*.in.json; do
+            echo -n "  Running $(basename ${f/.in.json/})..."
+            python proj.py $f ${f/.in.json/.out.json}
+            echo "Done"
+        done
+    # elif $1.in.json is in instances subdirectories
+    elif [ -f instances/*/$1.in.json ]; then
+        echo -n "Running $1..."
+        echo proj.py instances/${1/_*/}/$1.in.json instances/${1/_*/}/$1.out.json
+        echo "Done"
+    else
+        ran_tests=0
+    fi
+fi
+if [ $ran_tests == 0 ]; then
+    echo "Usage: ./test.sh [instance_group | short_instance_name]"
+    echo " e.g.: ./test.sh easy"
+    echo "       ./test.sh custom_toy_example"
+    echo "       ./test.sh hard_1"
+    echo "Available instance groups:"
+    echo "  easy, medium, hard, custom"
+    exit 1
+else
+    echo "Done"
+    exit 0
+fi
+
+
