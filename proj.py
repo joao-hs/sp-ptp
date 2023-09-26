028cd8563280d72659f4f2ff3e3b98070a325564
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2023-09-26 19:11:40 +0100

    refactor: restructuring and comments feat: initial input parsing and injecting values for minizinc variables

--- proj.py
+++ proj.py
@@ -1,11 +1,24 @@
 from json import load, dump
 from sys import argv
 from minizinc import Model, Solver, Instance
-from numpy import array
+
+
+# -----------------------------------------------------------------------------
+# ----------------------------- Support Functions -----------------------------
+# -----------------------------------------------------------------------------
 
 # Converts a time in the format HHhMM to minutes
 def get_minutes(time : str) -> int:
     return int(time[:2])*60+int(time[3:])
+
+# Converts a time in the format HHhMM:HHhMM to [minutes,minutes]
+def get_availability(time : str) -> list:
+    return [int(time[:2])*60+int(time[3:5]), int(time[6:8])*60+int(time[9:])]
+
+
+# -----------------------------------------------------------------------------
+# ----------------------------------- Setup -----------------------------------
+# -----------------------------------------------------------------------------
 
 input_name = argv[1]
 output_name = argv[2]
@@ -19,41 +32,47 @@ model = Model("./ptp.mzn")
 solver = Solver.lookup("gecode")
 instance = Instance(solver, model)
 
-# Converts a time in the format HHhMM:HHhMM to minutes:minutes
-def get_availability(time : str) -> int:
-    return (int(time[:2])*60+int(time[3:5]), int(time[6:8])*60+int(time[9:]))
 
-noVehicles = len(data["vehicles"])
-
-vehicleData = dict(
-    vehicleCanTake = [0]*noVehicles,
-    vehicleStart = [0]*noVehicles,
-    vehicleEnd = [0]*noVehicles,
-    vehicleCapacity = [0]*noVehicles,
-    vehicleAvailability = [0]*noVehicles,
-)
-
-noShifts = 0
-
-for index, v in enumerate(data["vehicle"]):
-    vehicleData["vehicleCanTake"][index] = v["canTake"]
-    vehicleData["vehicleStart"][index] = v["start"]
-    vehicleData["vehicleEnd"][index] = v["end"]
-    vehicleData["vehicleCapacity"][index] = v["capacity"]
-    vehicleData["vehicleAvailability"][index] = [get_availability(i) for i in v["availability"]]
-    noShifts = max(noShifts, len(v["availability"]))
-
-for key in vehicleData:
-    instance[key] = vehicleData[key]
-
-instance["noVehicles"] = noVehicles
-instance["noShifts"] = noShifts
+# -----------------------------------------------------------------------------
+# ------------------------ Parse Input & Assign Values ------------------------
+# -----------------------------------------------------------------------------
 
 instance["sameVehicleBackward"] = data["sameVehicleBackward"]
 instance["maxWaitTime"] = get_minutes(data["maxWaitTime"])
 
 noPlaces = len(data["places"])
 instance["noPlaces"] = noPlaces
+instance["placeCategory"] = [place["category"] for place in data["places"]]
+
+noVehicles = len(data["vehicles"])
+instance["noVehicles"] = noVehicles
+
+vehicleData = dict(
+    vehicleCanTake = [0]*noVehicles,
+    vehicleStart = [0]*noVehicles,
+    vehicleEnd = [0]*noVehicles,
+    vehicleCapacity = [0]*noVehicles,
+    vehicleAvailability = [[[0,0]]]*noVehicles,
+)
+
+noShifts = 0
+noCategories = 0
+
+for index, vehicle in enumerate(data["vehicles"]):
+    vehicleData["vehicleCanTake"][index] = vehicle["canTake"]
+    noCategories = max(noCategories, len(vehicle["canTake"]))
+    vehicleData["vehicleStart"][index] = vehicle["start"]
+    vehicleData["vehicleEnd"][index] = vehicle["end"]
+    vehicleData["vehicleCapacity"][index] = vehicle["capacity"]
+    vehicleData["vehicleAvailability"][index] = [get_availability(i) for i in vehicle["availability"]]
+    noShifts = max(noShifts, len(vehicle["availability"]))
+
+instance["noShifts"] = noShifts
+instance["noCategories"] = noCategories
+
+for key in vehicleData:
+    instance[key] = vehicleData[key]
+
 noRequests = len(data["patients"])
 instance["noRequests"] = noRequests
 
@@ -81,9 +100,13 @@ for index, patient in enumerate(data["patients"]):
 for key in requestData:
     instance[key] = requestData[key]
 
-instance["noPlaces"] = noPlaces
 instance["distMatrix"] = data["distMatrix"]
 
-result = instance.solve()
+
+# -----------------------------------------------------------------------------
+# --------------------------- Solve & Parse Output ----------------------------
+# -----------------------------------------------------------------------------
+
+result = instance.solve()   
 
 print(result)
