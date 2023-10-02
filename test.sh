36f6cbb6cbc1dc47db858850f400e6040f95704b
João Sereno <joaohsereno@tecnico.ulisboa.pt>
2023-10-02 11:30:15 +0100

    refactor: improvement to testing script

--- test.sh
+++ test.sh
@@ -5,44 +5,44 @@ ran_tests=1
 if [ -z "$1" ]; then
     echo "Running all tests"
     for f in instances/*/*.in.json; do
-        echo -n "  Running $(basename ${f/.in.json/})..."
+        echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
         python proj.py $f ${f/.in.json/.out.json}
-        echo "Done"
+        echo "TEST DONE ---------------------------------------------------"
     done
 elif [ $# == 1 ]; then
     if [ $1 == "easy" ]; then
         echo "Running easy tests"
         for f in instances/easy/*.in.json; do
-            echo -n "  Running $(basename ${f/.in.json/})..."
+            echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
             python proj.py $f ${f/.in.json/.out.json}
-            echo "Done"
+            echo "TEST DONE ---------------------------------------------------"
         done
     elif [ $1 == "medium" ]; then
         echo "Running medium tests"
         for f in instances/medium/*.in.json; do
-            echo -n "  Running $(basename ${f/.in.json/})..."
+            echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
             python proj.py $f ${f/.in.json/.out.json}
-            echo "Done"
+            echo "TEST DONE ---------------------------------------------------"
         done
     elif [ $1 == "hard" ]; then
         echo "Running hard tests"
         for f in instances/hard/*.in.json; do
-            echo -n "  Running $(basename ${f/.in.json/})..."
+            echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
             python proj.py $f ${f/.in.json/.out.json}
-            echo "Done"
+            echo "TEST DONE ---------------------------------------------------"
         done
     elif [ $1 == "custom" ]; then
         echo "Running custom tests"
         for f in instances/custom/*.in.json; do
-            echo -n "  Running $(basename ${f/.in.json/})..."
+            echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
             python proj.py $f ${f/.in.json/.out.json}
-            echo "Done"
+            echo "TEST DONE ---------------------------------------------------"
         done
     # elif $1.in.json is in instances subdirectories
     elif [ -f instances/*/$1.in.json ]; then
-        echo -n "Running $1..."
+        echo "OUTPUT FOR TEST: $(basename ${f/.in.json/})------------------"
         echo proj.py instances/${1/_*/}/$1.in.json instances/${1/_*/}/$1.out.json
-        echo "Done"
+        echo "TEST DONE ---------------------------------------------------"
     else
         ran_tests=0
     fi
